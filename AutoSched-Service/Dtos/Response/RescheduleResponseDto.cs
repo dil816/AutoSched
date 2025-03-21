403297e8863454dbbcca31d988e4b6d70f773ed4
dil816 <Rivindudilochana18329@gmail.com>
2025-03-21 21:32:56 +0530

    type changes

--- AutoSched-Service/Dtos/Response/RescheduleResponseDto.cs
+++ AutoSched-Service/Dtos/Response/RescheduleResponseDto.cs
@@ -15,7 +15,7 @@
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
-        public DateTimeOffset StartTime { get; set; }
-        public DateTimeOffset EndTime { get; set; }
+        public string StartTime { get; set; } = string.Empty;
+        public string EndTime { get; set; } = string.Empty;
     }
 }
