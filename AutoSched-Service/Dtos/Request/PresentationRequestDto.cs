268e8d1abc2d12f9f185d3b8b26adef1402f71b8
dil816 <Rivindudilochana18329@gmail.com>
2025-03-21 22:04:05 +0530

    conflits remove

--- AutoSched-Service/Dtos/Request/PresentationRequestDto.cs
+++ AutoSched-Service/Dtos/Request/PresentationRequestDto.cs
@@ -5,11 +5,11 @@
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
-<<<<<<< HEAD
+
         public string StartTime { get; set; } = string.Empty;
-=======
-        public string StartTime { get; set; } = string.Empty ;
->>>>>>> 1b1395a9eaa27fa45949ffe07003be34a19c837d
+
+   
+
         public string EndTime { get; set; } = string.Empty;
     }
 }
